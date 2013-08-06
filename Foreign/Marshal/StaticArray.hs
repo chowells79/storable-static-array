f0bdc63e7f879596fcc7a5ca16550b2a7913082a
Carl Howells <carl@janrain.com>
2013-08-05 19:37:53 -0700

    Rework module structure - docs not finished

--- Foreign/Marshal/StaticArray.hs
+++ Foreign/Marshal/StaticArray.hs
@@ -1,12 +1,9 @@
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE RecursiveDo #-}
-{-# LANGUAGE UndecidableInstances #-}
 {-|
 
 This module defines 'StaticArray', a simple wrapper around instances
@@ -44,26 +41,21 @@ module Foreign.Marshal.StaticArray
        , alignment'
        , poke'
        , peek'
-         -- * Adding new StaticSize instances
-         -- $NewStaticSize
-       , fromNat
-       , StaticSize(..)
        ) where
 
-import GHC.TypeLits
-
 import Control.Monad
+import Data.Functor ((<$>))
 
-import Data.Array            (Array)
+import Data.Array (Array)
 import Data.Array.Base
-import Data.Array.IO  hiding (unsafeFreeze)
-import Data.Functor          ((<$>))
-import Data.Proxy
+import Data.Array.IO hiding (unsafeFreeze)
+
 import Data.Tagged
 
-import Foreign.Storable      (Storable(..))
-import Foreign.Marshal.Array (advancePtr)
-import Foreign.Ptr           (Ptr, castPtr)
+import Foreign.Ptr
+import Foreign.Storable
+import Foreign.Marshal.Array
+import Foreign.Marshal.StaticSize
 
 
 -- | A minimal array wrapper that encodes the full dimensions of the
@@ -79,7 +71,7 @@ newtype StaticArray backing dimensions (elements :: *) =
         }
     deriving Eq
 
-instance (IArray b e, Ix (Bound d), Show e) => Show (StaticArray b d e) where
+instance (IArray b e, StaticSize d, Show e) => Show (StaticArray b d e) where
     show = ("listStaticArray " ++) . show . elems . toArray
 
 -- | Get the compile-time bounds from a 'StaticArray'. Does not examine its
@@ -94,15 +86,13 @@ staticBounds _ = untag (extent :: Tagged d (Bound d, Bound d))
 -- the bounds are as good as those provided by the 'StaticSize'
 -- instance.
 {-# INLINEABLE staticArray #-}
-staticArray :: (Ix (Bound d), IArray b e, StaticSize d) =>
-               [(Bound d, e)] -> StaticArray b d e
+staticArray :: (IArray b e, StaticSize d) => [(Bound d, e)] -> StaticArray b d e
 staticArray ls = let a = StaticArray $ array (staticBounds a) ls in a
 
 -- | Create a new 'StaticArray' from a list of elements in index
--- order. Implemented in terms of 'listArray'.
+-- order. Implemented in terms of 'listArray', with the same caveats.
 {-# INLINEABLE listStaticArray #-}
-listStaticArray :: (StaticSize d, Ix (Bound d), IArray b e) =>
-                   [e] -> StaticArray b d e
+listStaticArray :: (StaticSize d, IArray b e) => [e] -> StaticArray b d e
 listStaticArray ls = let a = StaticArray $ listArray (staticBounds a) ls in a
 
 
@@ -124,7 +114,7 @@ listStaticArray ls = let a = StaticArray $ listArray (staticBounds a) ls in a
 -- | Get the size, in bytes, of the native representation of this
 -- 'StaticArray'.
 {-# INLINEABLE sizeOf' #-}
-sizeOf' :: forall b d e. (StaticSize d, Ix (Bound d), Storable e) =>
+sizeOf' :: forall b d e. (StaticSize d, Storable e) =>
            StaticArray b d e -> Int
 sizeOf' a = sizeOf (undefined :: e) * rangeSize (staticBounds a)
 
@@ -137,23 +127,21 @@ alignment' _ = alignment (undefined :: e)
 -- | Write the contents of this 'StaticArray' to the given location in
 -- memory.
 {-# INLINEABLE poke' #-}
-poke' :: forall b d e. (Ix (Bound d), IArray b e, Storable e) =>
+poke' :: forall b d e. (StaticSize d, IArray b e, Storable e) =>
          Ptr (StaticArray b d e) -> StaticArray b d e -> IO ()
-poke' dst' arr = do
-        let a = toArray arr
-            b = bounds a
-            dst = castPtr dst'
-        forM_ [0 .. rangeSize b - 1] $ \i ->
-            poke (advancePtr dst i) $ unsafeAt a i
+poke' dst' (StaticArray a) = do
+    let upper = rangeSize (bounds a) - 1
+        dst = castPtr dst'
+    forM_ [0..upper] $ \i -> poke (advancePtr dst i) $ unsafeAt a i
 
 -- | Create a new 'StaticArray' from the contents of the given
 -- location in memory. Uses a temporary mutable array to build up the
--- result, then freezes it. The first argument is the
--- freezing function. Non-copying implementations of 'unsafeFreeze'
--- are safe as this argument, and preferred.
+-- result, then freezes it. The first argument is the freezing
+-- function. Non-copying implementations of 'unsafeFreeze' are safe as
+-- this argument, and preferred.
 {-# INLINEABLE peek' #-}
-peek' :: forall b d e m . (StaticSize d, Ix (Bound d), Storable e,
-                           IArray b e, MArray m e IO) =>
+peek' :: forall b d e m. (StaticSize d, Storable e, IArray b e,
+                          MArray m e IO) =>
          (m (Bound d) e -> IO (b (Bound d) e)) ->
          Ptr (StaticArray b d e) ->
          IO (StaticArray b d e)
@@ -169,8 +157,7 @@ peek' freeze' src' = do
         arr <- StaticArray <$> freeze' m
     return arr
 
-instance (StaticSize d, Ix (Bound d), Storable e,
-          IArray UArray e, MArray IOUArray e IO) =>
+instance (StaticSize d, Storable e, IArray UArray e, MArray IOUArray e IO) =>
          Storable (StaticArray UArray d e) where
     {-# INLINEABLE sizeOf#-}
     sizeOf = sizeOf'
@@ -182,8 +169,7 @@ instance (StaticSize d, Ix (Bound d), Storable e,
     peek = peek' (unsafeFreeze :: IOUArray (Bound d) e ->
                                   IO (UArray (Bound d) e))
 
-instance (StaticSize d, Ix (Bound d), Storable e) =>
-         Storable (StaticArray Array d e) where
+instance (StaticSize d, Storable e) => Storable (StaticArray Array d e) where
     {-# INLINEABLE sizeOf #-}
     sizeOf = sizeOf'
     {-# INLINEABLE alignment #-}
@@ -193,223 +179,3 @@ instance (StaticSize d, Ix (Bound d), Storable e) =>
     {-# INLINEABLE peek #-}
     peek = peek' (unsafeFreeze :: IOArray (Bound d) e ->
                                   IO (Array (Bound d) e))
-
-
-------------------------------------------------------------------------
--- $NewStaticSize
---
--- This module contains instances of 'StaticSize' for types of kind
--- 'Nat', types of the promoted kind \'['Nat'], and promoted tuples of
--- 'Nat' up to 13 elements. For instances not relying on promoted data
--- types, see the "Foreign.Marshal.StaticArray.Unpromoted" module.
-
--- | A conversion function for converting type-level naturals to
--- value-level. This is being exposed to aid in the creation of
--- additional 'StaticSize' instances for those who might desire to do
--- so.
---
--- Haddock is currently eating the important qualification that the
--- type variable @n@ must have the kind 'Nat'. The 'SingI' instance is
--- automatically fulfilled for all types of kind 'Nat'. Its explicit
--- presence in the signature is an artifact of how GHC implements
--- dictionary passing and type erasure.
-fromNat :: forall (proxy :: Nat -> *) (n :: Nat). SingI n => proxy n -> Int
-fromNat _ = fromInteger $ fromSing (sing :: Sing n)
-
--- | This class connects dimension description types with 'IArray'
--- index types and values.
-class StaticSize d where
-    -- | The bounding type for this dimension description
-    type Bound d :: *
-    -- | The concrete bounds for an array of this
-    -- dimensionality, tagged with the dimensionality.
-    extent :: Tagged d (Bound d, Bound d)
-
-instance SingI n => StaticSize ('[n] :: [Nat]) where
-    type Bound ('[n]) = Int
-    extent = Tagged (0, fromNat (Proxy :: Proxy n) - 1)
-
-instance (SingI n, StaticSize (n2 ': ns)) =>
-          StaticSize ((n ': n2 ': ns) :: [Nat]) where
-    type Bound (n ': n2 ': ns) = (Int, Bound (n2 ': ns))
-    extent = Tagged ((0, b0), (fromNat (Proxy :: Proxy n) - 1, bn))
-      where
-        (b0, bn) = untag (extent :: Tagged (n2 ': ns)
-                                           (Bound (n2 ': ns),
-                                            Bound (n2 ': ns)))
-
-instance SingI a => StaticSize (a :: Nat) where
-    type Bound a = Int
-    extent = Tagged (0, fromNat (Proxy :: Proxy a) - 1)
-
-instance (SingI a, SingI b) => StaticSize ('(a, b) :: (Nat, Nat)) where
-    type Bound '(a, b) = (Int, Int)
-    extent = Tagged ((0, 0),
-                     (fromNat (Proxy :: Proxy a) - 1,
-                     fromNat (Proxy :: Proxy b) - 1))
-
-instance (SingI a, SingI b, SingI c) =>
-         StaticSize ('(a, b, c) :: (Nat, Nat, Nat)) where
-    type Bound '(a, b, c) = (Int, Int, Int)
-    extent = Tagged ((0, 0, 0),
-                     (fromNat (Proxy :: Proxy a) - 1,
-                      fromNat (Proxy :: Proxy b) - 1,
-                      fromNat (Proxy :: Proxy c) - 1))
-
-instance (SingI a, SingI b, SingI c, SingI d) =>
-         StaticSize ('(a, b, c, d) :: (Nat, Nat, Nat, Nat)) where
-    type Bound '(a, b, c, d) = (Int, Int, Int, Int)
-    extent = Tagged ((0, 0, 0, 0),
-                     (fromNat (Proxy :: Proxy a) - 1,
-                      fromNat (Proxy :: Proxy b) - 1,
-                      fromNat (Proxy :: Proxy c) - 1,
-                      fromNat (Proxy :: Proxy d) - 1))
-
-instance (SingI a, SingI b, SingI c, SingI d, SingI e) =>
-         StaticSize ('(a, b, c, d, e) :: (Nat, Nat, Nat, Nat, Nat)) where
-    type Bound '(a, b, c, d, e) = (Int, Int, Int, Int, Int)
-    extent = Tagged ((0, 0, 0, 0, 0),
-                     (fromNat (Proxy :: Proxy a) - 1,
-                      fromNat (Proxy :: Proxy b) - 1,
-                      fromNat (Proxy :: Proxy c) - 1,
-                      fromNat (Proxy :: Proxy d) - 1,
-                      fromNat (Proxy :: Proxy e) - 1))
-
-instance (SingI a, SingI b, SingI c, SingI d, SingI e, SingI f) =>
-         StaticSize ('(a, b, c, d, e, f) ::
-                     (Nat, Nat, Nat, Nat, Nat, Nat)) where
-    type Bound '(a, b, c, d, e, f) = (Int, Int, Int, Int, Int, Int)
-    extent = Tagged ((0, 0, 0, 0, 0, 0),
-                     (fromNat (Proxy :: Proxy a) - 1,
-                      fromNat (Proxy :: Proxy b) - 1,
-                      fromNat (Proxy :: Proxy c) - 1,
-                      fromNat (Proxy :: Proxy d) - 1,
-                      fromNat (Proxy :: Proxy e) - 1,
-                      fromNat (Proxy :: Proxy f) - 1))
-
-instance (SingI a, SingI b, SingI c, SingI d, SingI e, SingI f, SingI g) =>
-         StaticSize ('(a, b, c, d, e, f, g) ::
-                     (Nat, Nat, Nat, Nat, Nat, Nat, Nat)) where
-    type Bound '(a, b, c, d, e, f, g) = (Int, Int, Int, Int, Int, Int, Int)
-    extent = Tagged ((0, 0, 0, 0, 0, 0, 0),
-                     (fromNat (Proxy :: Proxy a) - 1,
-                      fromNat (Proxy :: Proxy b) - 1,
-                      fromNat (Proxy :: Proxy c) - 1,
-                      fromNat (Proxy :: Proxy d) - 1,
-                      fromNat (Proxy :: Proxy e) - 1,
-                      fromNat (Proxy :: Proxy f) - 1,
-                      fromNat (Proxy :: Proxy g) - 1))
-
-instance (SingI a, SingI b, SingI c, SingI d, SingI e, SingI f, SingI g,
-          SingI h) =>
-         StaticSize ('(a, b, c, d, e, f, g, h) ::
-                     (Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat)) where
-    type Bound '(a, b, c, d, e, f, g, h) =
-        (Int, Int, Int, Int, Int, Int, Int, Int)
-    extent = Tagged ((0, 0, 0, 0, 0, 0, 0, 0),
-                     (fromNat (Proxy :: Proxy a) - 1,
-                      fromNat (Proxy :: Proxy b) - 1,
-                      fromNat (Proxy :: Proxy c) - 1,
-                      fromNat (Proxy :: Proxy d) - 1,
-                      fromNat (Proxy :: Proxy e) - 1,
-                      fromNat (Proxy :: Proxy f) - 1,
-                      fromNat (Proxy :: Proxy g) - 1,
-                      fromNat (Proxy :: Proxy h) - 1))
-
-instance (SingI a, SingI b, SingI c, SingI d, SingI e, SingI f, SingI g,
-          SingI h, SingI i) =>
-         StaticSize ('(a, b, c, d, e, f, g, h, i) ::
-                     (Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat)) where
-    type Bound '(a, b, c, d, e, f, g, h, i) =
-        (Int, Int, Int, Int, Int, Int, Int, Int, Int)
-    extent = Tagged ((0, 0, 0, 0, 0, 0, 0, 0, 0),
-                     (fromNat (Proxy :: Proxy a) - 1,
-                      fromNat (Proxy :: Proxy b) - 1,
-                      fromNat (Proxy :: Proxy c) - 1,
-                      fromNat (Proxy :: Proxy d) - 1,
-                      fromNat (Proxy :: Proxy e) - 1,
-                      fromNat (Proxy :: Proxy f) - 1,
-                      fromNat (Proxy :: Proxy g) - 1,
-                      fromNat (Proxy :: Proxy h) - 1,
-                      fromNat (Proxy :: Proxy i) - 1))
-
-instance (SingI a, SingI b, SingI c, SingI d, SingI e, SingI f, SingI g,
-          SingI h, SingI i, SingI j) =>
-         StaticSize ('(a, b, c, d, e, f, g, h, i, j) ::
-                     (Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat)) where
-    type Bound '(a, b, c, d, e, f, g, h, i, j) =
-        (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)
-    extent = Tagged ((0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-                     (fromNat (Proxy :: Proxy a) - 1,
-                      fromNat (Proxy :: Proxy b) - 1,
-                      fromNat (Proxy :: Proxy c) - 1,
-                      fromNat (Proxy :: Proxy d) - 1,
-                      fromNat (Proxy :: Proxy e) - 1,
-                      fromNat (Proxy :: Proxy f) - 1,
-                      fromNat (Proxy :: Proxy g) - 1,
-                      fromNat (Proxy :: Proxy h) - 1,
-                      fromNat (Proxy :: Proxy i) - 1,
-                      fromNat (Proxy :: Proxy j) - 1))
-
-instance (SingI a, SingI b, SingI c, SingI d, SingI e, SingI f, SingI g,
-          SingI h, SingI i, SingI j, SingI k) =>
-         StaticSize ('(a, b, c, d, e, f, g, h, i, j, k) ::
-                     (Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat))
-      where
-    type Bound '(a, b, c, d, e, f, g, h, i, j, k) =
-        (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)
-    extent = Tagged ((0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-                     (fromNat (Proxy :: Proxy a) - 1,
-                      fromNat (Proxy :: Proxy b) - 1,
-                      fromNat (Proxy :: Proxy c) - 1,
-                      fromNat (Proxy :: Proxy d) - 1,
-                      fromNat (Proxy :: Proxy e) - 1,
-                      fromNat (Proxy :: Proxy f) - 1,
-                      fromNat (Proxy :: Proxy g) - 1,
-                      fromNat (Proxy :: Proxy h) - 1,
-                      fromNat (Proxy :: Proxy i) - 1,
-                      fromNat (Proxy :: Proxy j) - 1,
-                      fromNat (Proxy :: Proxy k) - 1))
-
-instance (SingI a, SingI b, SingI c, SingI d, SingI e, SingI f, SingI g,
-          SingI h, SingI i, SingI j, SingI k, SingI l) =>
-         StaticSize ('(a, b, c, d, e, f, g, h, i, j, k, l) ::
-                     (Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat,
-                      Nat)) where
-    type Bound '(a, b, c, d, e, f, g, h, i, j, k, l) =
-        (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)
-    extent = Tagged ((0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-                     (fromNat (Proxy :: Proxy a) - 1,
-                      fromNat (Proxy :: Proxy b) - 1,
-                      fromNat (Proxy :: Proxy c) - 1,
-                      fromNat (Proxy :: Proxy d) - 1,
-                      fromNat (Proxy :: Proxy e) - 1,
-                      fromNat (Proxy :: Proxy f) - 1,
-                      fromNat (Proxy :: Proxy g) - 1,
-                      fromNat (Proxy :: Proxy h) - 1,
-                      fromNat (Proxy :: Proxy i) - 1,
-                      fromNat (Proxy :: Proxy j) - 1,
-                      fromNat (Proxy :: Proxy k) - 1,
-                      fromNat (Proxy :: Proxy l) - 1))
-
-instance (SingI a, SingI b, SingI c, SingI d, SingI e, SingI f, SingI g,
-          SingI h, SingI i, SingI j, SingI k, SingI l, SingI m) =>
-         StaticSize ('(a, b, c, d, e, f, g, h, i, j, k, l, m) ::
-                     (Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat, Nat,
-                      Nat, Nat)) where
-    type Bound '(a, b, c, d, e, f, g, h, i, j, k, l, m) =
-        (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)
-    extent = Tagged ((0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-                     (fromNat (Proxy :: Proxy a) - 1,
-                      fromNat (Proxy :: Proxy b) - 1,
-                      fromNat (Proxy :: Proxy c) - 1,
-                      fromNat (Proxy :: Proxy d) - 1,
-                      fromNat (Proxy :: Proxy e) - 1,
-                      fromNat (Proxy :: Proxy f) - 1,
-                      fromNat (Proxy :: Proxy g) - 1,
-                      fromNat (Proxy :: Proxy h) - 1,
-                      fromNat (Proxy :: Proxy i) - 1,
-                      fromNat (Proxy :: Proxy j) - 1,
-                      fromNat (Proxy :: Proxy k) - 1,
-                      fromNat (Proxy :: Proxy l) - 1,
-                      fromNat (Proxy :: Proxy m) - 1))
---
+++ Foreign/Marshal/StaticVector.hs
@@ -0,0 +1,85 @@
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RecursiveDo #-}
+{-|
+
+X
+
+-}
+module Foreign.Marshal.StaticVector where
+
+import Control.Monad
+import Data.Functor ((<$>))
+
+import Data.Ix
+
+import qualified Data.Vector.Generic         as VG
+import qualified Data.Vector.Generic.Mutable as VGM
+
+import Data.Tagged
+
+import Foreign.Ptr
+import Foreign.Storable
+import Foreign.Marshal.Array
+import Foreign.Marshal.StaticSize
+
+
+newtype StaticVector backing dimensions (elements :: *) =
+    StaticVector {
+        -- | Returns the backing of this 'StaticVector'.
+        toVector :: backing elements
+        }
+    deriving (Eq)
+
+instance (VG.Vector b e, Show e) => Show (StaticVector b d e) where
+    show = ("fromList " ++) . show . VG.toList . toVector
+
+
+-- | Get the compile-time bounds from a 'StaticVector'. Does not examine its
+-- argument.
+{-# INLINEABLE staticBounds #-}
+staticBounds :: forall b d e. StaticSize d =>
+                StaticVector b d e -> (Bound d, Bound d)
+staticBounds _ = untag (extent :: Tagged d (Bound d, Bound d))
+
+{-# INLINEABLE staticSize #-}
+staticSize :: StaticSize d => StaticVector b d e -> Int
+staticSize = rangeSize . staticBounds
+
+{-# INLINEABLE fromList #-}
+fromList :: (VG.Vector b e, StaticSize d) => [e] -> StaticVector b d e
+fromList els | len == size = sv
+             | otherwise =
+                 error $ "wrong size list provided to fromList; expected " ++
+                 show size ++ ", got " ++ show len
+  where
+    len = length els
+    size = staticSize sv
+    sv = StaticVector $ VG.fromListN size els
+
+instance (StaticSize d, Storable e, VG.Vector b e) =>
+         Storable (StaticVector b d e) where
+    {-# INLINEABLE sizeOf #-}
+    sizeOf a = sizeOf (undefined :: e) * rangeSize (staticBounds a)
+    {-# INLINEABLE alignment #-}
+    alignment _ = alignment (undefined :: e)
+    {-# INLINEABLE poke #-}
+    poke dst' sv@(StaticVector v) = do
+        let upper = staticSize sv - 1
+            dst = castPtr dst'
+        forM_ [0..upper] $ \i -> poke (advancePtr dst i) $ VG.unsafeIndex v i
+    {-# INLINEABLE peek #-}
+    peek src' = do
+        rec let size = staticSize sv
+            v <- VGM.unsafeNew size
+
+            let src = castPtr src'
+            forM_ [0 .. size - 1] $ \i -> do
+                x <- peek $ advancePtr src i
+                VGM.unsafeWrite v i x
+
+            sv <- StaticVector <$> VG.unsafeFreeze v
+        return sv
